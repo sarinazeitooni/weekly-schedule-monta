97de6887f303ecba7f71899ef6c59d59fed300c1
sarinazeitooni <76405658+sarinazeitooni@users.noreply.github.com>
2021-01-02 17:07:17 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,38 +1,47 @@
-debugger
-
 let hours = "8:30 - 7:00";
 let container = document.getElementById("mainContainer");
 let dataContainer="";
-let items = document.getElementsByClassName("items");
+let items,bgColoritem;
 let data = [
     shanbe =[ {
+        type:"normal",
         day:"شنبه",
      name: "ادبیات فارسی" ,
     teacher: "محمدرضا خوش برخورد" ,
     hours: hours ,
     online :true,
     exam:false
-    }, {name: "زنگ تفریح" ,
+    }, {
+        type:"break",
+        name: "زنگ تفریح" ,
     teacher: "" ,
     hours: hours ,
     online :false,
     exam:false
-    },{name: "جبر و احتمال 3" ,
+    },{
+        type:"normal",
+        name: "جبر و احتمال 3" ,
     teacher: "رضا علوی" ,
     hours: hours ,
     online :false,
     exam:false
-    }, {name: "زنگ تفریح" ,
+    }, {
+        type:"break",
+        name: "زنگ تفریح" ,
     teacher: "" ,
     hours: hours ,
     online :false,
     exam:false
-    },{ name: "جبر و احتمال 3" ,
+    },{
+        type:"normal",
+        name: "جبر و احتمال 3" ,
     teacher: "رضا علوی" ,
     hours: hours ,
     online :true,
     exam:false
-    }, {name: "نهار و نماز" ,
+    }, {
+        type:"lunch",
+        name: "نهار و نماز" ,
     teacher: "" ,
     hours: hours ,
     online :false,
@@ -40,18 +49,23 @@ let data = [
     }],
 
     yekshanbe=[{
+        type:"normal",
         day:"یکشنبه",
         name: "جبر و احتمال 3" ,
     teacher: "رضا علوی" ,
     hours: hours ,
     online :false,
     exam:false
-    },{name: "زنگ تفریح" ,
+    },{
+        type:"break",
+        name: "زنگ تفریح" ,
     teacher: "" ,
     hours: hours ,
     online :false,
     exam:false
-    },{name: "آزمون ریاضی" ,
+    },{
+        type:"exam",
+        name: "آزمون ریاضی" ,
         teacher: "محمدرضا خوش برخورد" ,
         hours: hours ,
         online :false,
@@ -60,43 +74,58 @@ let data = [
 
     doshanbe=[
         {day:"دوشنبه",
-            name: "آزمون ریاضی" ,
+        type:"exam",
+         name: "آزمون ریاضی" ,
         teacher: "محمدرضا خوش برخورد" ,
         hours: hours ,
         online :false,
         exam: true
     },
-    {name: "زنگ تفریح" ,
+    {
+        type:"break",
+        name: "زنگ تفریح" ,
     teacher: "" ,
     hours: hours ,
     online :false,
     exam:false
-    },{name: "جبر و احتمال 3" ,
+    },{
+        type:"normal",
+        name: "جبر و احتمال 3" ,
     teacher: "رضا علوی" ,
     hours: hours ,
     online :false,
     exam:false
-    },{name: "جبر و احتمال 3" ,
+    },{
+        type:"normal",
+        name: "جبر و احتمال 3" ,
     teacher: "رضا علوی" ,
     hours: hours ,
     online :false,
     exam:false
-    },{name: "جبر و احتمال 3" ,
+    },{
+        type:"normal",
+        name: "جبر و احتمال 3" ,
     teacher: "رضا علوی" ,
     hours: hours ,
     online :false,
     exam:false
-    },{name: "جبر و احتمال 3" ,
+    },{
+        type:"normal",
+        name: "جبر و احتمال 3" ,
     teacher: "رضا علوی" ,
     hours: hours ,
     online :false,
     exam:false
-    },{name: "جبر و احتمال 3" ,
+    },{
+        type:"normal",
+        name: "جبر و احتمال 3" ,
     teacher: "رضا علوی" ,
     hours: hours ,
     online :false,
     exam:false
-    },{name: "جبر و احتمال 3" ,
+    },{
+        type:"normal",
+        name: "جبر و احتمال 3" ,
     teacher: "رضا علوی" ,
     hours: hours ,
     online :false,
@@ -106,36 +135,46 @@ let data = [
     ],
 
     seshanbe=[{
+        type:"normal",
         day:"سه شنبه",
         name: "جبر و احتمال 3" ,
     teacher: "رضا علوی" ,
     hours: hours ,
     online :false,
     exam:false
-    },{name: "زنگ تفریح" ,
+    },{
+        type:"break",
+        name: "زنگ تفریح" ,
     teacher: "" ,
     hours: hours ,
     online :false,
     exam:false
-    },{name: "آزمون ریاضی" ,
+    },{
+        type:"exam",
+        name: "آزمون ریاضی" ,
         teacher: "محمدرضا خوش برخورد" ,
         hours: hours ,
         online :false,
         exam: true
     }],
     cheharshanbe=[{
+        type:"normal",
         day:"چهارشنبه",
         name: "جبر و احتمال 3" ,
     teacher: "رضا علوی" ,
     hours: hours ,
     online :false,
     exam:false
-    },{name: "زنگ تفریح" ,
+    },{
+        type:"break",
+        name: "زنگ تفریح" ,
     teacher: "" ,
     hours: hours ,
     online :false,
     exam:false
-    },{name: "آزمون ریاضی" ,
+    },{
+        type:"exam",
+        name: "آزمون ریاضی" ,
         teacher: "محمدرضا خوش برخورد" ,
         hours: hours ,
         online :false,
@@ -143,12 +182,14 @@ let data = [
     }],
 
     panjshanbe=[{
+        type:"normal",
         day:"پنج شنبه",
         name: "زبان" ,
        teacher: "محمدرضا خوش برخورد" ,
        hours: hours ,
        online :false
        },{
+        type:"normal",
         name: "عربی" ,
        teacher: "محمدرضا خوش برخورد" ,
        hours: hours ,
@@ -160,21 +201,34 @@ let data = [
     };
     function calenderView(){
         // container.classList.add("flexContainer");
+        // items = document.getElementsByClassName("items");
         // items.classList.add("flexChildren");
         container.innerHTML = dataContainer;
     };
     data.map(daysConstructer);
     function daysConstructer(item){
-    dataContainer += `<div class=" items flexChildren">${item[0].day}<hr>
+    dataContainer += `<div class="items flexChildrenMain flexChildren" >${item[0].day}<hr>
     ${item.map(classConstructer)} </div>`;
     };
     function classConstructer(element){
-        return `<div class="flexChildren">${element.name}<br>${element.teacher}<br>${element.hours}</div>`;
+        return `<div class="flexChildren"><div style="background-color :${colorPicker(element)}; margin:0 auto; width:78%" class=" is-rounded text-size-12-n">${element.name}<br>${element.teacher}<br>${element.hours}</div></div>`;
     };
-
+function colorPicker(element){
+   if(element.type=="normal"){
+       bgColoritem = "#1faa0041";
+   }else if(element.type=="exam"){
+    bgColoritem = "#ffb30041";
+   }else if(element.type=="break"){
+    bgColoritem = "#00b0ff41"; 
+   }else if(element.type=="lunch"){
+    bgColoritem = "#5a779441"
+   }
+   return bgColoritem ;
+};
     
-    function listView(){
-        container.classList.remove("flexContainer");
-        items.classList.remove("flexChildren");
-        };
+    // function listView(){
+    //     items = document.getElementsByClassName("items");
+    //     container.classList.remove("flexContainer");
+    //     items.classList.remove("flexChildren");
+    //     };
     
