6088dadc0a9fa73d16ad91ca5b3d9ad8c57d6c44
sarinazeitooni <76405658+sarinazeitooni@users.noreply.github.com>
2021-01-04 16:14:15 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -56,36 +56,40 @@ function onlineCheck(element) {
 data.map(daysConstructer);
 //calenderView
 function calenderView(){
+    document.getElementById("calenderView").style.color = "black";
+    document.getElementById("listView").style.color = "rgba(131, 130, 130, 0.315)";
     container.style.border = "1.5px solid  rgba(131, 130, 130, 0.315)";
     deleterButton.style.visibility = "visible";
     container.classList.add("flexContainer");
     container.innerHTML = dataContainerCalender;
 };
 function listView(){
+    document.getElementById("calenderView").style.color = "rgba(131, 130, 130, 0.315)";
+    document.getElementById("listView").style.color = "black";
     deleterButton.style.visibility = "hidden";
     container.classList.remove("flexContainer");
     container.style.border = "none";
     container.innerHTML = dataContainerList;
 };
 function daysConstructer(item) {
-    dataContainerCalender += `<div class="flexChildrenMain"><div class="dayItemsmobile">${item[0].day}</div><hr><div class="">
+    dataContainerCalender += `<div class="flexChildrenMain"><div class=" space-top dayItemsmobile"><span class="is-hidden-widescreen tableIcon mi-Info"></span>${item[0].day}</div><hr><div class="">
     ${item.map(classConstructerCalender)} </div></div>`;
-      dataContainerList +=`<div class="has-text-right dayItems dayItemsmobile" >${item[0].day}</div><div>${item.map(classConstructerList)}</div>`;    
+      dataContainerList +=`<div style="" class="has-text-right dayItems dayItemsmobile" ><span class=" tableIcon mi-Info"></span>${item[0].day}</div><div>${item.map(classConstructerList)}</div>`;    
 };
 function classConstructerCalender(element) {
-    return `<div style="background-color :${colorPicker(element)};
-        margin:0 auto; width:78%" class=" is-rounded text-size-12-n"><span class=" icon mi-Info"></span>
+    return `<div style="background-color :${colorPicker(element)};position:relative;
+        margin:5px auto; width:90%; padding:10px;" class=" is-rounded text-size-10-n"><span class="deleteIcon icons mi-Info"></span>
         ${element.name}<br>${element.teacher}<br>${element.hours}<br><span class="${onlineCheck(element)}"></span></div>`;
 };
 function classConstructerList(element) {
-        return `<div style="background-color :${colorPicker(element)};
+        return `<div style=" background-color :${colorPicker(element)};
          width:100%" class=" columns is-rounded text-size-10-mobile-n text-size-12-n">
         <span class="column is-6-mobile is- space-64-left-n space-12-mobile-n"> ${element.name} </span>
-        <span class="column is-6-mobile space-64-right-n space-12-mobile-n space-64-left-n"> ${element.teacher} </span>
-        <span class="column is-6-mobile space-64-right-n space-64-left-n space-12-mobile-n"> ${element.hours} </span>
+        <span class="column is-6-mobile space-32-right-n space-12-mobile-n space-32-left-n"> ${element.teacher} </span>
+        <span class="column is-6-mobile space-32-right-n space-32-left-n space-12-mobile-n"> ${element.hours} </span>
         <span class="column is-6-mobile ${onlineCheck(element)} "></span>
         <div class="column is-12-mobile">
-        <span class="is-pulled-left icon mi-Info"></span><span class="is-pulled-left icon mi-Info"></span>
+        <span class="is-pulled-left is-hidden-mobile icons mi-Info"></span><span class="is-pulled-left is-hidden-mobile icons mi-Info"></span>
         </div>
         </div>`;
 };
