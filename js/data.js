79f5a2ba6df00d538b50199d874b6ee5db549651
sarinazeitooni <76405658+sarinazeitooni@users.noreply.github.com>
2021-01-09 13:17:52 -0800

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,206 @@
+let weeklyschedule = [{
+    day: "شنبه",
+    id: 1,
+    classDetail: [{
+            type: "normal",
+            name: "ادبیات فارسی",
+            teacher: "محمدرضا خوش برخورد",
+            hours: " 8:30 - 7:00 ",
+            online: true
+        },
+        {
+            type: "break",
+            name: "زنگ تفریح",
+            teacher: "",
+            teacher: "",
+            hours: " 8:30 - 7:00 ",
+            online: false
+        },
+        {
+            type: "normal",
+            name: "جبر و احتمال 3",
+            teacher: "رضا علوی",
+            hours: " 8:30 - 7:00 ",
+            online: false
+        }, {
+            type: "break",
+            name: "زنگ تفریح",
+            teacher: "",
+            teacher: "",
+            hours: " 8:30 - 7:00 ",
+            online: false
+        },
+        {
+            type: "normal",
+            name: "جبر و احتمال 3",
+            teacher: "رضا علوی",
+            hours: " 8:30 - 7:00 ",
+            online: true
+        },
+        {
+            type: "lunch",
+            name: "نهار و نماز",
+            teacher: "",
+            hours: " 8:30 - 7:00 ",
+            online: false
+        }
+    ]
+},
+{
+    day: "یکشنبه",
+    id: 2,
+    classDetail: [{
+            type: "normal",
+            name: "جبر و احتمال 3",
+            teacher: "رضا علوی",
+            hours: " 8:30 - 7:00 ",
+            online: false
+        },
+        {
+            type: "break",
+            name: "زنگ تفریح",
+            teacher: "",
+            hours: " 8:30 - 7:00 ",
+            online: false
+        },
+        {
+            type: "exam",
+            name: "آزمون ریاضی",
+            teacher: "محمدرضا خوش برخورد",
+            hours: " 8:30 - 7:00 ",
+            online: false
+        }
+    ]
+},
+{
+    day: "دوشنبه",
+    id: 3,
+    classDetail: [
+
+        {
+            type: "exam",
+            name: "آزمون ریاضی",
+            teacher: "محمدرضا خوش برخورد",
+            hours: " 8:30 - 7:00 ",
+            online: false
+        },
+        {
+            type: "break",
+            name: "زنگ تفریح",
+            teacher: "",
+            hours: " 8:30 - 7:00 ",
+            online: false
+        }, {
+            type: "normal",
+            name: "جبر و احتمال 3",
+            teacher: "رضا علوی",
+            hours: " 8:30 - 7:00 ",
+            online: false
+        },
+        {
+            type: "normal",
+            name: "جبر و احتمال 3",
+            teacher: "رضا علوی",
+            hours: " 8:30 - 7:00 ",
+            online: false
+        }, {
+            type: "normal",
+            name: "جبر و احتمال 3",
+            teacher: "رضا علوی",
+            hours: " 8:30 - 7:00 ",
+            online: false
+        },
+        {
+            type: "normal",
+            name: "جبر و احتمال 3",
+            teacher: "رضا علوی",
+            hours: " 8:30 - 7:00 ",
+            online: false,
+            exam: false
+        }, {
+            type: "normal",
+            name: "جبر و احتمال 3",
+            teacher: "رضا علوی",
+            hours: " 8:30 - 7:00 ",
+            online: false
+        }, {
+            type: "normal",
+            name: "جبر و احتمال 3",
+            teacher: "رضا علوی",
+            hours: " 8:30 - 7:00 ",
+            online: false
+        }
+    ]
+},
+{
+    day: "سه شنبه",
+    id: 4,
+    classDetail: [
+
+        {
+            type: "normal",
+            name: "جبر و احتمال 3",
+            teacher: "رضا علوی",
+            hours: " 8:30 - 7:00 ",
+            online: false
+        }, {
+            type: "break",
+            name: "زنگ تفریح",
+            teacher: "",
+            hours: " 8:30 - 7:00 ",
+            online: false
+        }, {
+            type: "exam",
+            name: "آزمون ریاضی",
+            teacher: "محمدرضا خوش برخورد",
+            hours: " 8:30 - 7:00 ",
+            online: false
+        }
+    ]
+},
+{
+    day: "چهارشنبه",
+    id: 5,
+    classDetail: [{
+            type: "normal",
+            name: "جبر و احتمال 3",
+            teacher: "رضا علوی",
+            hours: " 8:30 - 7:00 ",
+            online: false
+        },
+        {
+            type: "break",
+            name: "زنگ تفریح",
+            teacher: "",
+            hours: " 8:30 - 7:00 ",
+            online: false
+        },
+        {
+            type: "exam",
+            name: "آزمون ریاضی",
+            teacher: "محمدرضا خوش برخورد",
+            hours: " 8:30 - 7:00 ",
+            online: false
+        }
+    ]
+},
+{
+    day: "پنج شنبه",
+    id: 6,
+    classDetail: [{
+            type: "normal",
+            name: "زبان",
+            teacher: "محمدرضا خوش برخورد",
+            hours: " 8:30 - 7:00   ",
+            online: false
+        },
+        {
+            type: "normal",
+            name: "عربی",
+            teacher: "محمدرضا خوش برخورد",
+            hours: " 8:30 - 7:00 ",
+            online: false
+        }
+    ]
+}
+];
--- js/main.js
+++ js/main.js
@@ -1,95 +1,104 @@
-let hours = "8:30 - 7:00";
-let container = document.getElementById("mainContainer");
-let deleterButton = document.getElementById("deleter");
-let dataContainerCalender,dataContainerList,bgColoritem;
-let data = [shanbe = [{ type: "normal", day: "شنبه",name: "ادبیات فارسی", teacher: "محمدرضا خوش برخورد", hours: hours, online: true },
-{ type: "break", name: "زنگ تفریح",teacher: "",hours: hours,online: false },
-{type: "normal",name: "جبر و احتمال 3",teacher: "رضا علوی",
-hours: hours,online: false }, {type: "break",name: "زنگ تفریح",teacher: "",hours: hours,online: false },
-{type: "normal",name: "جبر و احتمال 3",teacher: "رضا علوی",hours: hours, online: true },
-{  type: "lunch", name: "نهار و نماز", teacher: "", hours: hours,online: false}],
-yekshanbe = [{ type: "normal",day: "یکشنبه",name: "جبر و احتمال 3",teacher: "رضا علوی",hours: hours,online: false},
-{type: "break", name: "زنگ تفریح",teacher: "",hours: hours,online: false},
-{type: "exam",name: "آزمون ریاضی",teacher: "محمدرضا خوش برخورد",hours: hours,online: false}],
- doshanbe = [{day: "دوشنبه",type: "exam",name: "آزمون ریاضی",teacher: "محمدرضا خوش برخورد",hours: hours,online: false },
-{type: "break", name: "زنگ تفریح",teacher: "",hours: hours,online: false}, {type: "normal",name: "جبر و احتمال 3",teacher: "رضا علوی",hours: hours,online: false},
-{type: "normal", name: "جبر و احتمال 3",teacher: "رضا علوی",hours: hours,online: false},{type: "normal", name: "جبر و احتمال 3",teacher: "رضا علوی",hours: hours,online: false },
-{type: "normal",name: "جبر و احتمال 3",teacher: "رضا علوی",hours: hours, online: false,exam: false
-}, {type: "normal",name: "جبر و احتمال 3",teacher: "رضا علوی",hours: hours,online: false
-}, {type: "normal",name: "جبر و احتمال 3",teacher: "رضا علوی",hours: hours,online: false}],
-seshanbe = [{type: "normal", day: "سه شنبه", name: "جبر و احتمال 3",teacher: "رضا علوی",hours: hours,online: false
-}, {type: "break",name: "زنگ تفریح",teacher: "",hours: hours,online: false
- }, {type: "exam",name: "آزمون ریاضی",teacher: "محمدرضا خوش برخورد",hours: hours,online: false}],
-cheharshanbe = [{type: "normal",day: "چهارشنبه",name: "جبر و احتمال 3",teacher: "رضا علوی",hours: hours,online: false},
- {type: "break",name: "زنگ تفریح",teacher: "",hours: hours,online: false},
-{type: "exam",name: "آزمون ریاضی",teacher: "محمدرضا خوش برخورد",hours: hours,online: false}],
-panjshanbe = [{type: "normal",day: "پنج شنبه",name: "زبان",teacher: "محمدرضا خوش برخورد",hours: hours,online: false},
-{type: "normal",name: "عربی",teacher: "محمدرضا خوش برخورد",hours: hours,online: false }]];
-function initialData() {
-    calenderView();
+let container = document.getElementById("maincontainer");
+    let deleterButton = document.getElementById("deleter");
+    let dataContainerCalender = "";
+    let dataContainerList = "";
+    let bgColoritem;
+    function initialData() {
+    listView();
+    };
+    function calenderView() {
+document.getElementById("calenderView").style.color = "black";
+document.getElementById("listView").style.color = "rgba(131, 130, 130, 0.315)";
+container.style.border = "1.5px solid  rgba(131, 130, 130, 0.315)";
+deleterButton.style.visibility = "visible";
+container.classList.add("flexcontainer");
+container.innerHTML = dataContainerCalender;
 };
-function colorPicker(element) {
-    switch(element.type) {
+
+function listView() {
+document.getElementById("calenderView").style.color = "rgba(131, 130, 130, 0.315)";
+document.getElementById("listView").style.color = "black";
+deleterButton.style.visibility = "hidden";
+container.classList.remove("flexcontainer");
+container.style.border = "none";
+container.innerHTML = dataContainerList;
+};
+    function colorPicker(element) {
+    switch (element.type) {
         case "normal":
             bgColoritem = "rgba(31, 170, 0, 0.1)";
-          break;
+            break;
         case "exam":
             bgColoritem = "rgba(255, 179, 0, 0.1)";
-          break;
-          case "break":
+            break;
+        case "break":
             bgColoritem = "rgba(0, 176, 255, 0.1)";
-          break;
-          case "lunch":
+            break;
+        case "lunch":
             bgColoritem = "rgba(170, 0, 255, 0.1)";
-          break;
-          default: "normal"
-      }
+            break;
+        default:
+            "normal"
+    }
     return bgColoritem;
 };
-function onlineCheck(element) {
+function onlineCheckList(element) {
     if (element.online) {
-        return "mi-Info";
+        return `<span class=" mi-Info " style=" text-align: right; display:block; padding: 3px; margin: 10px;"><span class="is-hidden-mobile">آنلاین</span></span>`;
     } else {
         return "";
     }
 };
-data.map(daysConstructer);
-//calenderView
-function calenderView(){
-    document.getElementById("calenderView").style.color = "black";
-    document.getElementById("listView").style.color = "rgba(131, 130, 130, 0.315)";
-    container.style.border = "1.5px solid  rgba(131, 130, 130, 0.315)";
-    deleterButton.style.visibility = "visible";
-    container.classList.add("flexContainer");
-    container.innerHTML = dataContainerCalender;
+function onlineCheckCalendar(element) {
+    if (element.online) {
+        return `<span class=" mi-Info " style=" text-align: right; display:block; padding: 3px; margin:0 10px;"></span>`;
+    } else {
+        return "";
+    }
 };
-function listView(){
-    document.getElementById("calenderView").style.color = "rgba(131, 130, 130, 0.315)";
-    document.getElementById("listView").style.color = "black";
-    deleterButton.style.visibility = "hidden";
-    container.classList.remove("flexContainer");
-    container.style.border = "none";
-    container.innerHTML = dataContainerList;
-};
+weeklyschedule.map(daysConstructer);
 function daysConstructer(item) {
-    dataContainerCalender += `<div class="flexChildrenMain"><div class=" space-top dayItemsmobile"><span class="is-hidden-widescreen tableIcon mi-Info"></span>${item[0].day}</div><hr><div class="">
-    ${item.map(classConstructerCalender)} </div></div>`;
-      dataContainerList +=`<div style="" class="has-text-right dayItems dayItemsmobile" ><span class=" tableIcon mi-Info"></span>${item[0].day}</div><div>${item.map(classConstructerList)}</div>`;    
+    dataContainerCalender += `<div class=" flexchildrenmain"><div class="space-top dayItemsmobile">
+        <span class="is-hidden-widescreen tableicon mi-Info"></span>${item.day}</div>
+        <hr>
+        <div style="margin:3px auto;">
+    ${item.classDetail.map(classConstructerCalender)} </div></div>`;
+    dataContainerList += `<div style="" class="has-text-right dayItems dayItemsmobile">
+        <span class=" tableicon mi-Calendar1"></span>
+        ${item.day}</div><div>${item.classDetail.map(classConstructerList)}</div>`;
+
 };
 function classConstructerCalender(element) {
     return `<div style="background-color :${colorPicker(element)};position:relative;
-        margin:5px auto; width:80%; padding:10px;" class=" is-rounded text-size-10-n"><span class="deleteIcon icons mi-Info"></span>
-        ${element.name}<br>${element.teacher}<br>${element.hours}<br><span class="${onlineCheck(element)}"></span></div>`;
+        margin:3px 15px; padding:15px 5px;" class=" is-rounded mobiletest text-size-10-n">
+        <span class=" deleteicon icons mi-Delete-X-icon---16px"></span>
+       <span class="has-text-weight-bolder column" style="padding:1px"> ${element.name}</span>
+       <span class='text-grey-n' style='display:block; padding: 3px;' >${element.teacher}</span>
+       <span class='text-grey-n' style='display:block;'>${element.hours}</span>
+       <span style="position:absolute; right:-5px; top:75%;" class="is-pulled-right">${onlineCheckCalendar(element)}</span></div>`;
 };
+ function teacherCheck(element){
+    if(element!== ""){
+        return `<span class="column width50-mobile" style="text-align: right; margin: 10px;">` + element + `</span>`;
+    }else
+    {
+        return `<span class=" column is-hidden-mobile">  </span>`;
+    }
+ };
 function classConstructerList(element) {
-        return `<div style=" background-color :${colorPicker(element)};
-         width:100%" class=" columns is-rounded text-size-10-mobile-n text-size-12-n">
-        <span class="column is-6-mobile is- space-64-left-n space-12-mobile-n"> ${element.name} </span>
-        <span class="column is-6-mobile space-32-right-n space-12-mobile-n space-32-left-n"> ${element.teacher} </span>
-        <span class="column is-6-mobile space-32-right-n space-32-left-n space-12-mobile-n"> ${element.hours} </span>
-        <span class="column is-6-mobile ${onlineCheck(element)} "></span>
-        <div class="column is-12-mobile">
-        <span class="is-pulled-left is-hidden-mobile icons mi-Info"></span><span class="is-pulled-left is-hidden-mobile icons mi-Info"></span>
+
+  return  `<div style="flex-wrap:wrap; background-color :${colorPicker(element)};" class="columns fullwidth is-rounded margin-mobile is-flex-mobile text-size-10-mobile-n text-size-12-n">
+        <span class="column width50-mobile has-text-weight-bolder" style=" text-align: right; margin: 10px;" > <span class="mi-No-answer"></span> ${element.name} </span>
+        ${teacherCheck(element.teacher)}
+          <span class="column width50-mobile text-grey-n" style='line-height: inherit; text-align: right; padding: 3px; margin: 10px;'><span>${element.hours}</span><span class="mi-Clock-icon"></span></span>
+        ${onlineCheckList(element)}
+        <div class=" column is-hidden-mobile">
+        <span style="background-color: white; border-radius: 20px; padding: 1px; margin:1px;" class="is-pulled-left icons mi-ic-edit">
+            <span class='path1'></span><span class='path2'></span>
+                 </span>
+        <span style="background-color: white; border-radius: 20px; padding: 1px; margin:1px;" class="is-pulled-left icons mi-ic-trash"">
+            <span class="path1"></span><span class="path2"></span>
+                 </span>
         </div>
         </div>`;
 };
